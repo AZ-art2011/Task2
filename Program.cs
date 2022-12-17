830df1efbfdee86d7658a4b2240fca2e430e3525
Alexander <mypost@example.com>
2022-12-17 14:05:26 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2
+
+Console.WriteLine("Вас приветствует программа сравнения двух чисел");
+
+Console.WriteLine("Введите первое число");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if (numFirst > numSecond){
+    max = numFirst;
+    min = numSecond;
+}
+else
+{
+    max = numSecond;
+    min = numFirst;
+}
+
+Console.WriteLine($"Число {max} - большее");
+Console.WriteLine($"Число {min} - меньшее");
